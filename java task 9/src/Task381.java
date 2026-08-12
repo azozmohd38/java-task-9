96c3b288b0d37bf84f4c6008afb55bd3cf33da9b
Abdulaziz <azozmohd38@gmail.com>
2026-08-12 16:31:51 +0400

    task381 (Power of 2 Check)

---
+++ java task 9/src/Task381.java
@@ -0,0 +1,16 @@
+public class Task381 {
+    public static boolean isPowerOfTwo(int n) {
+        if (n == 1) {
+            return true;
+        }
+        if (n <= 0 || n % 2 != 0) {
+            return false;
+        }
+        return isPowerOfTwo(n / 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPowerOfTwo(16));
+        System.out.println(isPowerOfTwo(20)); 
+    }
+}
