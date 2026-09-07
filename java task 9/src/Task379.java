43350f246f52c5434504d4a561ddfaff2315ade0
azozmohd38 <azozmohd38@gmail.com>
2026-09-07 15:44:25 +0400

    rename task379 text

--- java task 9/src/Task379.java
+++ java task 9/src/Task379.java
@@ -1,9 +1,9 @@
 public class Task379 {
-    public static int length(String str) {
-        if (str.isEmpty()) {
+    public static int length(String text) {
+        if (text.isEmpty()) {
             return 0;
         }
-        return 1 + length(str.substring(1));
+        return 1 + length(text.substring(1));
     }
 
     public static void main(String[] args) {
