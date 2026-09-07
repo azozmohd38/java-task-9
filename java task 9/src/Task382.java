05aef18b7058c7a32d58e0b7236a5fe67bd0c32d
azozmohd38 <azozmohd38@gmail.com>
2026-09-07 15:45:43 +0400

    rename task382 number

--- java task 9/src/Task382.java
+++ java task 9/src/Task382.java
@@ -1,13 +1,13 @@
 public class Task382 {
-    public static boolean isEven(int n) {
-        n = Math.abs(n);
-        if (n == 0) {
+    public static boolean isEven(int number) {
+        number = Math.abs(number);
+        if (number == 0) {
             return true;
         }
-        if (n == 1) {
+        if (number == 1) {
             return false;
         }
-        return isEven(n - 2);
+        return isEven(number - 2);
     }
 
     public static void main(String[] args) {
