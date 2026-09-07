665139f576578aa5e507d7ba8a2393b7e3acc9d6
azozmohd38 <azozmohd38@gmail.com>
2026-09-07 15:43:28 +0400

    clean task378 code

--- java task 9/src/Task378.java
+++ java task 9/src/Task378.java
@@ -1,12 +1,12 @@
 public class Task378 {
     public static int multiply(int a, int b) {
         if (b == 0) {
-            return 0;              // Base case
+            return 0;
         }
         return a + multiply(a, b - 1);
     }
 
     public static void main(String[] args) {
-        IO.println(multiply(3, 4)); // 12
+        IO.println(multiply(3, 4));
     }
 }
