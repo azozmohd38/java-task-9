1baee9c0ed2a7aa052dc3b5f013754b1b67a0de3
azozmohd38 <azozmohd38@gmail.com>
2026-09-07 15:48:49 +0400

    add task387 recursion

--- java task 9/src/Task387.java
+++ java task 9/src/Task387.java
@@ -1,2 +1,10 @@
 public class Task387 {
+
+    public static int countVowels(String text) {
+        if (text.isEmpty()) {
+            return 0;
+        }
+
+        return countVowels(text.substring(1));
+    }
 }
