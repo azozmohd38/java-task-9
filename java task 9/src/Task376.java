c6a1fa95e6668518ea7e8085d236fddc360f580f
azozmohd38 <azozmohd38@gmail.com>
2026-09-07 15:42:45 +0400

    add task376 examples

--- java task 9/src/Task376.java
+++ java task 9/src/Task376.java
@@ -7,7 +7,13 @@ public class Task376 {
         System.out.println(number);
     }
 
-    public static void main(String[] args) {
+    private static void runExamples() {
+        printUp(3);
+        System.out.println("---");
         printUp(5);
+    }
+
+    public static void main(String[] args) {
+        runExamples();
     }
 }
