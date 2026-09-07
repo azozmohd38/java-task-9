bf4746c1abac81aeb111b7adaf21e0c965b48c5e
azozmohd38 <azozmohd38@gmail.com>
2026-09-07 15:46:39 +0400

    fix task384 print

--- java task 9/src/Task384.java
+++ java task 9/src/Task384.java
@@ -8,6 +8,6 @@ public class Task384 {
     }
 
     public static void main(String[] args) {
-        IO.println(countChar("banana", 'a'));
+        System.out.println(countChar("banana", 'a'));
     }
 }
